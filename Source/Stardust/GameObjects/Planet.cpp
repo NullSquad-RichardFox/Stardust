4a60663ca225543ff994bc32187c1cc6ca8e930e
NullSquad-RichardFox <richardfox.aurora@gmail.com>
2023-01-08 07:42:33 +0100

    Planet generation
    Creating planet generation function

--- Source/Stardust/GameObjects/Planet.cpp
+++ Source/Stardust/GameObjects/Planet.cpp
@@ -2,4 +2,188 @@
 
 
 #include "Planet.h"
+#include "Camera/CameraComponent.h"
+#include "Stardust/GameModes/GameFramework.h"
 
+
+
+APlanet::APlanet()
+{
+	WidgetCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	WidgetCamera->SetupAttachment(RootComponent);
+}
+
+void APlanet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CreatePlanet();
+}
+
+void APlanet::OnDayUpdate()
+{
+	Super::OnDayUpdate();
+
+	PartialPopulation += PopulationGrowth / 30;
+	if (PartialPopulation >= PopulationGrowthThreshold)
+	{
+		Population++;
+		PartialPopulation -= PopulationGrowthThreshold;
+	}
+}
+
+void APlanet::OnMonthUpdate()
+{
+	Super::OnMonthUpdate();
+}
+
+void APlanet::OnClicked()
+{
+	Super::OnClicked();
+
+	UE_LOG(LogTemp, Warning, TEXT("[%s][%i]: OnClicked"), *GetName(), __LINE__);
+
+}
+
+
+
+void APlanet::CreatePlanet()
+{
+	constexpr int32 MaxFeaturesPerBuildSlot = 4;
+
+	//Object declarations
+	AGameFramework* GameMode = Cast<AGameFramework>(GetWorld()->GetAuthGameMode());
+	UEnum* FeatureTypeEnum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EFeatureType"), true);
+
+
+	if (!FeatureTypeEnum)
+		return;
+
+	if (!GameMode)
+		return;
+
+	//if (!TerrestrialMaterial || !OceanMaterial || !BarrenMaterial)
+	//	return;
+
+
+	//Sets random planet type
+	if (UEnum* PlanetTypeEnum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EPlanetType"), true))
+	{
+		PlanetType = EPlanetType(FMath::RandRange(0, (int32)PlanetTypeEnum->GetMaxEnumValue() - 1));
+
+		switch (PlanetType)
+		{
+		case EPlanetType::Ocean:
+			//MeshComponent->SetMaterial(0, OceanMaterial);
+			break;
+
+		case EPlanetType::Barren:
+			//MeshComponent->SetMaterial(0, BarrenMaterial);
+			break;
+
+		case EPlanetType::Terrestrial:
+			//MeshComponent->SetMaterial(0, TerrestrialMaterial);
+			break;
+		}
+	}
+	else PlanetType = EPlanetType::Ocean;
+
+	//Sets planet size range
+	FVector2D PlanetSizeRange(UStructDataLibrary::GetData(PlanetType)->Range);
+	PlanetSize = FMath::RandRange((int32)PlanetSizeRange.X, (int32)PlanetSizeRange.Y);
+
+	//Creates build slots
+	BuildSlots.Reserve(PlanetSize);
+	for (int32 i = 0; i < PlanetSize; i++)
+		BuildSlots.Add(FBuildSlot());
+
+	//Generates features
+	for (int32 i = 0; i < FeatureTypeEnum->GetMaxEnumValue(); i++)
+	{
+		if (UStructDataLibrary::GetData((EFeatureType)i)->bPlanetary)
+		{
+			GeneratePlanetaryFeature(i);
+			continue;
+		}
+
+		for (FBuildSlot& BuildSlot : BuildSlots)
+			GenerateFeature(i, BuildSlot);
+	}
+
+	//RecalculateModifiers();
+
+	//Logging
+	UE_LOG(LogTemp, Warning, TEXT("--------------------"))
+	UE_LOG(LogTemp, Warning, TEXT("PlanetType: [%s]"), *UEnum::GetValueAsString(PlanetType))
+	UE_LOG(LogTemp, Warning, TEXT("PlanetSize: [%i]"), PlanetSize)
+	UE_LOG(LogTemp, Warning, TEXT("--------------------"))
+}
+
+void APlanet::GenerateFeature(int32 FeatureIndex, FBuildSlot& BuildSlot)
+{
+	const FFeatureData* Feature = UStructDataLibrary::GetData((EFeatureType)FeatureIndex);
+
+	if (!Feature)
+		return;
+
+	if (!Feature->RequiredType.Contains(PlanetType) && Feature->RequiredType.Num() > 0)
+		return;
+
+	if ((Feature->RequiredSize.X > PlanetSize || PlanetSize > Feature->RequiredSize.Y) && (Feature->RequiredSize.Y - Feature->RequiredSize.X > 0))
+		return;
+
+	bool bPrerequisites = true;
+	for (EFeatureType PrerequisiteType : Feature->Prerequisites)
+		bPrerequisites = (bool)(bPrerequisites * BuildSlot.Features.Contains(PrerequisiteType));
+
+	if (!bPrerequisites)
+		return;
+
+	float Chance = Feature->Rarity;
+	bool bGenerated;
+
+	do
+	{
+		bGenerated = FMath::FRand() <= Chance;
+		Chance = Chance * Chance * Chance;
+
+		if (bGenerated)
+		{
+			BuildSlot.Features.Add((EFeatureType)FeatureIndex);
+
+			for (const EDistrictModifier& Modifier : Feature->DistrictModifiers)
+				BuildSlot.District.AddModifier(Modifier);
+		}
+
+	} while (bGenerated && Feature->bMultiple);
+}
+
+void APlanet::GeneratePlanetaryFeature(int32 FeatureIndex)
+{
+	const FFeatureData* Feature = UStructDataLibrary::GetData((EFeatureType)FeatureIndex);
+
+	if (!Feature)
+		return;
+
+	if (!Feature->RequiredType.Contains(PlanetType) && Feature->RequiredType.Num() > 0)
+		return;
+
+	if ((Feature->RequiredSize.X > PlanetSize || PlanetSize > Feature->RequiredSize.Y) && (Feature->RequiredSize.Y - Feature->RequiredSize.X > 0))
+		return;
+
+	float Chance = Feature->Rarity;
+	bool bGenerated;
+
+	do
+	{
+		bGenerated = FMath::FRand() <= Chance;
+		Chance = Chance * Chance * Chance;
+
+		if (bGenerated)
+		{
+			PlanetaryFeatures.Add((EFeatureType)FeatureIndex);
+			PlanetaryModifiers.Append(Feature->PlanetModifiers);
+		}
+
+	} while (bGenerated && Feature->bMultiple);
+}
--- Source/Stardust/GameObjects/Planet.h
+++ Source/Stardust/GameObjects/Planet.h
@@ -4,14 +4,56 @@
 
 #include "CoreMinimal.h"
 #include "GameActor.h"
+#include "Stardust/CoreDataStructs.h"
 #include "Planet.generated.h"
 
-/**
- * 
- */
+
+class UCameraComponent;
+
 UCLASS()
 class STARDUST_API APlanet : public AGameActor
 {
 	GENERATED_BODY()
 	
+public:
+	APlanet();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void OnDayUpdate() override;
+	virtual void OnMonthUpdate() override;
+	virtual void OnClicked() override;
+
+private:
+	void CreatePlanet();
+	void GenerateFeature(int32 FeatureIndex, FBuildSlot& BuildSlot);
+	void GeneratePlanetaryFeature(int32 FeatureIndex);
+
+private:
+	UPROPERTY(EditAnywhere)
+	UCameraComponent* WidgetCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Infrastructure")
+	int32 MaxBuildStackCount;
+	UPROPERTY(VisibleAnywhere, Category = "Infrastructure")
+	TArray<FBuildSlot> BuildSlots;
+
+	UPROPERTY(VisibleAnywhere, Category = "Infrastructure|Population")
+	int32 Population;
+	UPROPERTY(VisibleAnywhere, Category = "Infrastructure|Population")
+	float PopulationGrowth;
+	UPROPERTY(VisibleAnywhere, Category = "Infrastructure|Population")
+	float PopulationGrowthThreshold = MAX_FLT;
+
+	UPROPERTY(VisibleAnywhere, Category = "Planet")
+	int32 PlanetSize;
+	UPROPERTY(VisibleAnywhere, Category = "Planet")
+	EPlanetType PlanetType;
+	UPROPERTY(VisibleAnywhere, Category = "Planet")
+	TArray<EFeatureType> PlanetaryFeatures;
+	UPROPERTY(VisibleAnywhere, Category = "Planet")
+	TArray<EPlanetModifier> PlanetaryModifiers;
+
+	float PartialPopulation;
+	float UnemployedPopulation;
 };
