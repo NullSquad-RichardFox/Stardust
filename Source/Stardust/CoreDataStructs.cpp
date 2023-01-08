2ac54d0fda9dd5d75edbf43703f26d04681613f7
NullSquad-RichardFox <richardfox.aurora@gmail.com>
2023-01-08 07:41:49 +0100

    Creating basic data types

--- Source/Stardust/CoreDataStructs.cpp
+++ Source/Stardust/CoreDataStructs.cpp
@@ -3,3 +3,336 @@
 
 #include "CoreDataStructs.h"
 
+
+
+UStructDataLibrary::UStructDataLibrary()
+{
+	DistrictDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_DistrictData.DT_DistrictData'");
+	BuildingDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_BuildingData.DT_BuildingData'");
+	JobDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_JobData.DT_JobData'");
+	DistrictModifiersDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_DistrictModifierData.DT_DistrictModifierData'");
+	PlanetModifiersDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_PlanetModifierData.DT_PlanetModifierData'");
+	FeatureDataTable = LoadDataTable("DataTable'/Game/DataTables/DT_FeaturesData.DT_FeaturesData'");
+}
+
+UDataTable* UStructDataLibrary::LoadDataTable(const FName& FilePath)
+{
+	return Cast<UDataTable>(StaticLoadObject(UDataTable::StaticClass(), NULL, *FilePath.ToString()));
+}
+
+const FPlanetSizeRangeData* UStructDataLibrary::GetData(EPlanetType PlanetType)
+{
+	if (!PlanetSizeRangeDataTable) return nullptr;
+
+	UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EPlanetType"), true);
+
+	if (!Enum) return nullptr;
+
+
+	FString ContextString("UStructDataLibrary");
+	return PlanetSizeRangeDataTable->FindRow<FPlanetSizeRangeData>(*Enum->GetNameStringByValue((int32)PlanetType), ContextString);
+}
+
+const FBuildingData* UStructDataLibrary::GetData(EBuildingType BuildingType)
+{
+	if (!BuildingDataTable) return nullptr;
+
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EBuildingType"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return BuildingDataTable->FindRow<FBuildingData>(*Enum->GetNameStringByValue((int32)BuildingType), ContextString);
+
+	}
+
+	return nullptr;
+}
+
+const FDistrictData* UStructDataLibrary::GetData(EDistrictType DistrictType, int32 DistrictTier)
+{
+	if (!DistrictDataTable) return nullptr;
+
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EDistrictType"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return DistrictDataTable->FindRow<FDistrictData>(*(Enum->GetNameStringByIndex((int32)DistrictType) + "_" + FString::FromInt(DistrictTier)), ContextString);
+	}
+
+	return nullptr;
+}
+
+const FJobData* UStructDataLibrary::GetData(EJobType JobType)
+{
+	if (!JobDataTable) return nullptr;
+
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EJobType"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return JobDataTable->FindRow<FJobData>(*Enum->GetNameStringByIndex((int32)JobType), ContextString);
+	}
+
+	return nullptr;
+}
+
+const FModifierData* UStructDataLibrary::GetData(EDistrictModifier ModifierType)
+{
+	if (!DistrictModifiersDataTable) return nullptr;
+
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EDistrictModifier"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return DistrictModifiersDataTable->FindRow<FModifierData>(*Enum->GetNameStringByIndex((int32)ModifierType), ContextString);
+	}
+
+	return nullptr;
+}
+
+const FModifierData* UStructDataLibrary::GetData(EPlanetModifier ModifierType)
+{
+	if (!PlanetModifiersDataTable) return nullptr;
+
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EPlanetModifier"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return PlanetModifiersDataTable->FindRow<FModifierData>(*Enum->GetNameStringByIndex((int32)ModifierType), ContextString);
+	}
+
+	return nullptr;
+}
+
+const FFeatureData* UStructDataLibrary::GetData(EFeatureType FeatureType)
+{
+	if (!FeatureDataTable)
+	{
+		return nullptr;
+		UE_LOG(LogTemp, Error, TEXT("DataTableNotFound"))
+	}
+	if (UEnum* Enum = FindObject<UEnum>(ANY_PACKAGE, TEXT("EFeatureType"), true))
+	{
+		FString ContextString("UStructDataLibrary");
+		return FeatureDataTable->FindRow<FFeatureData>(*Enum->GetNameStringByIndex((int32)FeatureType), ContextString);
+	}
+
+	return nullptr;
+}
+
+
+
+
+TMap<EResourceType, float> FDistrict::GetDistrictProduction() const
+{
+	TMap<EResourceType, float> JobProduction;
+
+	for (const TPair<EJobType, int32>& Job : OccupiedJobs)
+		for (const TPair<EResourceType, float>& Resource : UStructDataLibrary::GetData(Job.Key)->ResourceProduction)
+			JobProduction.FindOrAdd(Resource.Key) += Resource.Value * Job.Value;
+
+	for (const TPair<EResourceType, float>& Modifier : ProductionModifiers)
+		if (JobProduction.Contains(Modifier.Key))
+			JobProduction[Modifier.Key] *= Modifier.Value;
+
+	return JobProduction;
+}
+
+TMap<EResourceType, float> FDistrict::GetDistrictUpkeep() const
+{
+	TMap<EResourceType, float> JobUpkeep;
+
+	for (const EBuildingType& Building : Buildings)
+		for (const TPair<EResourceType, float>& BuildingUpkeep : UStructDataLibrary::GetData(Building)->ResourceUpkeep)
+			JobUpkeep.FindOrAdd(BuildingUpkeep.Key) += BuildingUpkeep.Value;
+
+	for (const TPair<EJobType, int32>& Job : OccupiedJobs)
+		for (const TPair<EResourceType, float>& Resource : UStructDataLibrary::GetData(Job.Key)->ResourceUpkeep)
+			JobUpkeep.FindOrAdd(Resource.Key) += Resource.Value * Job.Value;
+
+	for (const TPair<EResourceType, float>& Modifier : UpkeepModifiers)
+		if (JobUpkeep.Contains(Modifier.Key))
+			JobUpkeep[Modifier.Key] *= Modifier.Value;
+
+	return JobUpkeep;
+}
+
+TMap<EResourceType, float> FDistrict::AddUpProductionModifiers()
+{
+	TMap<EResourceType, float> Modifiers;
+
+	for (const EDistrictModifier& Modifier : ResourceModifiers)
+		for (const TPair<EResourceType, float>& ResourceModifier : UStructDataLibrary::GetData(Modifier)->ProductionModifiers)
+			Modifiers.FindOrAdd(ResourceModifier.Key) += ResourceModifier.Value;
+
+	return Modifiers;
+}
+
+TMap<EResourceType, float> FDistrict::AddUpUpkeepModifiers()
+{
+	TMap<EResourceType, float> Modifiers;
+
+	for (const EDistrictModifier& Modifier : ResourceModifiers)
+		for (const TPair<EResourceType, float>& ResourceModifier : UStructDataLibrary::GetData(Modifier)->UpkeepModifiers)
+			Modifiers.FindOrAdd(ResourceModifier.Key) += ResourceModifier.Value;
+
+	return Modifiers;
+}
+
+EJobType FDistrict::GetDistrictDefaultJob(EDistrictType Type)
+{
+	switch (Type)
+	{
+	case EDistrictType::OreCollection:
+		return EJobType::OreMiner;
+
+	case EDistrictType::CarbonCollection:
+		return EJobType::CarbonMiner;
+
+	case EDistrictType::SandCollection:
+		return EJobType::SandMiner;
+
+	case EDistrictType::WaterCollection:
+		return EJobType::WaterMiner;
+
+	case EDistrictType::FoodCollection:
+		return EJobType::Farmer;
+
+	case EDistrictType::MetalsManufacture:
+		return EJobType::Forger;
+
+	case EDistrictType::GlassManufacture:
+		return EJobType::GlassWorker;
+
+	case EDistrictType::SiliconManufacture:
+		return EJobType::SiliconWorker;
+
+	case EDistrictType::ElectronicsManufacture:
+		return EJobType::ElectronicsWorker;
+
+	case EDistrictType::PowerProduction:
+		return EJobType::PhysicsEngineer;
+
+	default:
+		return EJobType::MaintenanceWorker;
+	}
+}
+
+void FDistrict::PopulateJob(EJobType Job)
+{
+	if (OccupiedJobs.FindRef(Job) < DistrictJobs.FindRef(Job))
+		OccupiedJobs.FindOrAdd(Job)++;
+
+}
+
+void FDistrict::FreeJob(EJobType Job)
+{
+	if (OccupiedJobs.Contains(Job))
+		OccupiedJobs[Job] = FMath::Clamp(OccupiedJobs[Job] - 1, 0, 10000);
+}
+
+void FDistrict::AddBuilding(EBuildingType BuildingType)
+{
+	Buildings.Add(BuildingType);
+
+	if (const FBuildingData* BuildingData = UStructDataLibrary::GetData(BuildingType))
+	{
+		for (const TPair<EJobType, int32>& Job : BuildingData->BuildingJobs)
+			DistrictJobs.FindOrAdd(Job.Key) += Job.Value;
+
+		EnergyProduction += BuildingData->EnergyProduction;
+		EnergyUpkeep += BuildingData->EnergyUpkeep;
+	}
+}
+
+void FDistrict::RemoveBuilding(EBuildingType BuildingType)
+{
+	Buildings.Remove(BuildingType);
+
+	if (const FBuildingData* BuildingData = UStructDataLibrary::GetData(BuildingType))
+	{
+		for (const TPair<EJobType, int32>& Job : BuildingData->BuildingJobs)
+			DistrictJobs.FindOrAdd(Job.Key) -= Job.Value;
+
+		EnergyProduction -= BuildingData->EnergyProduction;
+		EnergyUpkeep -= BuildingData->EnergyUpkeep;
+	}
+}
+
+void FDistrict::AddModifier(EDistrictModifier Modifier)
+{
+	ResourceModifiers.Emplace(Modifier);
+	ProductionModifiers = AddUpProductionModifiers();
+	UpkeepModifiers = AddUpUpkeepModifiers();
+}
+
+void FDistrict::RemoveModifier(EDistrictModifier Modifier)
+{
+	ResourceModifiers.Remove(Modifier);
+	ProductionModifiers = AddUpProductionModifiers();
+	UpkeepModifiers = AddUpUpkeepModifiers();
+}
+
+const TArray<EBuildingType>& FDistrict::GetBuildings() const
+{
+	return Buildings;
+}
+
+const TArray<EDistrictModifier>& FDistrict::GetModifiers() const
+{
+	return ResourceModifiers;
+}
+
+const TMap<EJobType, int32>& FDistrict::GetJobs() const
+{
+	return DistrictJobs;
+}
+
+const TMap<EJobType, int32>& FDistrict::GetOccupiedJobs() const
+{
+	return OccupiedJobs;
+}
+
+void FDistrict::UpgradeDistrict(EDistrictType Type)
+{
+	DistrictTier++;
+	DistrictType = Type;
+
+	if (const FDistrictData* DistrictData = UStructDataLibrary::GetData(DistrictType, DistrictTier))
+	{
+		EnergyProduction += DistrictData->EnergyProduction;
+		EnergyUpkeep += DistrictData->EnergyUpkeep;
+		BuildSlots += DistrictData->BuildSlots;
+
+		for (const TPair<EJobType, int32>& Job : DistrictData->DistrictJobs)
+		{
+			if (Job.Key == EJobType::DistrictDefault)
+			{
+				DistrictJobs.FindOrAdd(FDistrict::GetDistrictDefaultJob(DistrictType)) += Job.Value;
+			}
+			else
+			{
+				DistrictJobs.FindOrAdd(Job.Key) += Job.Value;
+			}
+		}
+	}
+}
+
+TOptional<EJobType> FDistrict::GetFreeJob() const
+{
+	for (const TPair<EJobType, int32>& Job : DistrictJobs)
+	{
+		if (OccupiedJobs.Contains(Job.Key))
+		{
+			if (OccupiedJobs[Job.Key] < DistrictJobs[Job.Key])
+			{
+				return Job.Key;
+			}
+			else
+			{
+				return {};
+			}
+		}
+		else
+		{
+			return Job.Key;
+		}
+	}
+
+	return {};
+}
--- Source/Stardust/CoreDataStructs.h
+++ Source/Stardust/CoreDataStructs.h
@@ -3,15 +3,376 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
+#include "Engine/DataTable.h"
 #include "CoreDataStructs.generated.h"
 
-/**
- * 
- */
+
+class AConnection;
+
+UENUM()
+enum class EResourceType
+{
+	Oxygen					UMETA(DisplayName = "Oxygen"),
+	Water					UMETA(DisplayName = "Water"),
+	Food					UMETA(DisplayName = "Food"),
+	Ore						UMETA(DisplayName = "Ore"),
+	Metals					UMETA(DisplayName = "Metals"),
+	Sand					UMETA(DisplayName = "Sand"),
+	Silicon					UMETA(DisplayName = "Silicon"),
+	Carbon					UMETA(DisplayName = "Carbon"),
+	Glass					UMETA(DisplayName = "Glass"),
+	Electronics				UMETA(DisplayName = "Electronics")
+};
+
+UENUM()
+enum class EPlanetType
+{
+	Ocean					UMETA(DisplayName = ""),
+	Barren					UMETA(DisplayName = ""),
+	Terrestrial				UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EFeatureType
+{
+	Default					UMETA(DisplayName = ""),
+	MetalDeposits			UMETA(DisplayName = ""),
+	CarbonReserves			UMETA(DisplayName = ""),
+	SpralingShores			UMETA(DisplayName = ""),
+	DeepOceans				UMETA(DisplayName = ""),
+	DenseForests			UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EBuildingType
+{
+	Greenhouse				UMETA(DisplayName = ""),
+	SolarPanels				UMETA(DisplayName = ""),
+	WindTurbines			UMETA(DisplayName = ""),
+	Bioreactor				UMETA(DisplayName = ""),
+	ElectrolysisReactor		UMETA(DisplayName = ""),
+	AdditionalMiningShafts	UMETA(DisplayName = ""),
+	ExtraFactorySpaces		UMETA(DisplayName = ""),
+	SecondaryReactorChamber	UMETA(DisplayName = ""),
+	TheatreSquare			UMETA(DisplayName = ""),
+	Zoo						UMETA(DisplayName = ""),
+	Aquarium				UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EJobType
+{
+	Forger					UMETA(DisplayName = ""),
+	GlassWorker				UMETA(DisplayName = ""),
+	SiliconWorker			UMETA(DisplayName = ""),
+	ElectronicsWorker		UMETA(DisplayName = ""),
+	Farmer					UMETA(DisplayName = ""),
+	OreMiner				UMETA(DisplayName = ""),
+	CarbonMiner				UMETA(DisplayName = ""),
+	SandMiner				UMETA(DisplayName = ""),
+	WaterMiner				UMETA(DisplayName = ""),
+	PhysicsEngineer			UMETA(DisplayName = ""),
+	MaintenanceWorker		UMETA(DisplayName = ""),
+	ElectrolysisEngineer	UMETA(DisplayName = ""),
+	Actor					UMETA(DisplayName = ""),
+	AnimalKeeper			UMETA(DisplayName = ""),
+	Administrator			UMETA(DisplayName = ""),
+	DistrictDefault			UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EDistrictType
+{
+	OreCollection			UMETA(DisplayName = ""),
+	CarbonCollection		UMETA(DisplayName = ""),
+	SandCollection			UMETA(DisplayName = ""),
+	WaterCollection			UMETA(DisplayName = ""),
+	FoodCollection			UMETA(DisplayName = ""),
+	MetalsManufacture		UMETA(DisplayName = ""),
+	GlassManufacture		UMETA(DisplayName = ""),
+	SiliconManufacture		UMETA(DisplayName = ""),
+	ElectronicsManufacture	UMETA(DisplayName = ""),
+	PowerProduction			UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EDistrictModifier
+{
+	Mod						UMETA(DisplayName = "")
+};
+
+UENUM()
+enum class EPlanetModifier
+{
+	Mod						UMETA(DisplayName = "")
+};
+
+
+
+
+
 UCLASS()
-class STARDUST_API UCoreDataStructs : public UObject
+class STARDUST_API UStructDataLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+public:
+	UStructDataLibrary();
+
+	static const FPlanetSizeRangeData* GetData(EPlanetType PlanetType);
+	static const FBuildingData* GetData(EBuildingType BuildingType);
+	static const FDistrictData* GetData(EDistrictType DistrictType, int32 DistrictTier);
+	static const FJobData* GetData(EJobType JobType);
+	static const FModifierData* GetData(EDistrictModifier ModifierType);
+	static const FModifierData* GetData(EPlanetModifier ModifierType);
+	static const FFeatureData* GetData(EFeatureType FeatureType);
+
+private:
+	UDataTable* LoadDataTable(const FName& FilePath);
+
+	static UDataTable* PlanetSizeRangeDataTable;
+	static UDataTable* DistrictDataTable;
+	static UDataTable* BuildingDataTable;
+	static UDataTable* JobDataTable;
+	static UDataTable* DistrictModifiersDataTable;
+	static UDataTable* PlanetModifiersDataTable;
+	static UDataTable* FeatureDataTable;
+};
+
+UDataTable* UStructDataLibrary::PlanetSizeRangeDataTable = nullptr;
+UDataTable* UStructDataLibrary::BuildingDataTable = nullptr;
+UDataTable* UStructDataLibrary::DistrictDataTable = nullptr;
+UDataTable* UStructDataLibrary::JobDataTable = nullptr;
+UDataTable* UStructDataLibrary::DistrictModifiersDataTable = nullptr;
+UDataTable* UStructDataLibrary::PlanetModifiersDataTable = nullptr;
+UDataTable* UStructDataLibrary::FeatureDataTable = nullptr;
+
+
+
+
+
+USTRUCT()
+struct FPlanetSizeRangeData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Construction")
+	FVector2D Range;
+};
+
+USTRUCT()
+struct FDistrictData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Construction")
+		float BuildTime;
+	UPROPERTY(EditAnywhere, Category = "Construction")
+		float BuildCost;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		float EnergyProduction;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		float EnergyUpkeep;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		int32 BuildSlots;
+	UPROPERTY(EditAnywhere, Category = "Jobs")
+		TMap<EJobType, int32> DistrictJobs;
+};
+
+USTRUCT()
+struct FBuildingData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category = "Construction")
+		TArray<EDistrictType> RequiredDistrict;
+	UPROPERTY(EditAnywhere, Category = "Construction")
+		float BuildTime;
+	UPROPERTY(EditAnywhere, Category = "Construction")
+		float BuildCost;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		float EnergyProduction;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		float EnergyUpkeep;
+	UPROPERTY(EditAnywhere, Category = "Infrastructure")
+		TMap<EResourceType, float> ResourceUpkeep;
+	UPROPERTY(EditAnywhere, Category = "Jobs")
+		TMap<EJobType, int32> BuildingJobs;
+};
+
+USTRUCT()
+struct FJobData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+		TMap<EResourceType, float> ResourceProduction;
+	UPROPERTY(EditAnywhere)
+		TMap<EResourceType, float> ResourceUpkeep;
+};
+
+USTRUCT()
+struct FModifierData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+		TMap<EResourceType, float> ProductionModifiers;
+	UPROPERTY(EditAnywhere)
+		TMap<EResourceType, float> UpkeepModifiers;
+};
+
+USTRUCT()
+struct FFeatureData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere)
+		bool bPlanetary;
+	UPROPERTY(EditAnywhere)
+		bool bMultiple;
+
+	UPROPERTY(EditAnywhere)
+		float Rarity;
+
+	UPROPERTY(EditAnywhere)
+		FVector2D RequiredSize;
+	UPROPERTY(EditAnywhere)
+		TArray<EPlanetType> RequiredType;
+	UPROPERTY(EditAnywhere)
+		TArray<EFeatureType> Prerequisites;
+
+	UPROPERTY(EditAnywhere)
+		TArray<EDistrictType> BuildableDistricts;
+
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "bPlanetary"))
+		TArray<EPlanetModifier> PlanetModifiers;
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "!bPlanetary"))
+		TArray<EDistrictModifier> DistrictModifiers;
+};
+
+USTRUCT()
+struct FDistrict
+{
+	GENERATED_BODY()
+
+public:
+	FDistrict()
+		:DistrictTier(0),
+		DistrictType(),
+		EnergyProduction(0.f),
+		EnergyUpkeep(0.f),
+		BuildSlots(0),
+		DistrictJobs({}),
+		Buildings({}),
+		ResourceModifiers({})
+	{}
+
+	FDistrict(int32 DistrictTier, EDistrictType DistrictType, float EnergyProduction, float EnergyUpkeep, int32 BuildSlots, TMap<EJobType, int32> DistrictJobs, TArray<EBuildingType> Buildings)
+		:DistrictTier(DistrictTier),
+		DistrictType(DistrictType),
+		EnergyProduction(EnergyProduction),
+		EnergyUpkeep(EnergyUpkeep),
+		BuildSlots(BuildSlots),
+		DistrictJobs(DistrictJobs),
+		Buildings(Buildings),
+		ResourceModifiers({})
+	{}
+
+	FDistrict(int32 DistrictTier, EDistrictType DistrictType, float EnergyProduction, float EnergyUpkeep, int32 BuildSlots, TMap<EJobType, int32> DistrictJobs, TArray<EBuildingType> Buildings, TArray<EDistrictModifier> ResourceModifiers)
+		:DistrictTier(DistrictTier),
+		DistrictType(DistrictType),
+		EnergyProduction(EnergyProduction),
+		EnergyUpkeep(EnergyUpkeep),
+		BuildSlots(BuildSlots),
+		DistrictJobs(DistrictJobs),
+		Buildings(Buildings),
+		ResourceModifiers(ResourceModifiers)
+	{}
+
+	TMap<EResourceType, float> GetDistrictProduction() const;
+	TMap<EResourceType, float> GetDistrictUpkeep() const;
+
+	void PopulateJob(EJobType Job);
+	void FreeJob(EJobType Job);
+
+	void AddBuilding(EBuildingType BuildingType);
+	void RemoveBuilding(EBuildingType BuildingType);
+
+	void AddModifier(EDistrictModifier Modifier);
+	void RemoveModifier(EDistrictModifier Modifier);
+
+	const TArray<EBuildingType>& GetBuildings() const;
+	const TArray<EDistrictModifier>& GetModifiers() const;
+	const TMap<EJobType, int32>& GetJobs() const;
+	const TMap<EJobType, int32>& GetOccupiedJobs() const;
+
+	void UpgradeDistrict(EDistrictType Type);
+
+	static EJobType GetDistrictDefaultJob(EDistrictType Type);
+
+	TOptional<EJobType> GetFreeJob() const;
+
+private:
+	TMap<EResourceType, float> AddUpProductionModifiers();
+	TMap<EResourceType, float> AddUpUpkeepModifiers();
+
+public:
+	UPROPERTY(VisibleAnywhere)
+		int32 DistrictTier;
+	UPROPERTY(VisibleAnywhere)
+		EDistrictType DistrictType;
+
+	UPROPERTY(VisibleAnywhere)
+		float EnergyProduction;
+	UPROPERTY(VisibleAnywhere)
+		float EnergyUpkeep;
+
+	UPROPERTY(VisibleAnywhere)
+		int32 BuildSlots;
+
+private:
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = true))
+		TMap<EJobType, int32> OccupiedJobs;
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = true))
+		TMap<EJobType, int32> DistrictJobs;
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = true))
+		TArray<EBuildingType> Buildings;
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = true))
+		TArray<EDistrictModifier> ResourceModifiers;
+
+	TMap<EResourceType, float> ProductionModifiers;
+	TMap<EResourceType, float> UpkeepModifiers;
+};
+
+struct FRoute
+{
+	TArray<TWeakPtr<AConnection>> RouteConnections;
+
+	float RouteDangerLevel;
+	float RouteLength;
+};
+
+USTRUCT()
+struct FBuildSlot
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere)
+		FDistrict District;
+	UPROPERTY(VisibleAnywhere)
+		TArray<EFeatureType> Features;
+
+	FBuildSlot()
+		:District(FDistrict()),
+		Features({})
+	{}
 };
